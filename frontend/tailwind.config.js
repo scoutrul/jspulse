72f2fffe6cf733f0a6729235ef09773d4cda5814
agolovachev <antongolova@gmail.com>
2025-04-25 16:47:02 +0700

    архитектурные фиксы

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,18 @@
+import { skeleton } from "@skeletonlabs/tw-plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{html,js,svelte,ts}' // Указываем пути для сканирования
+    "./src/**/*.{html,js,svelte,ts}", // Указываем пути для сканирования
+    // Добавляем путь к файлам Skeleton UI
+    require("path").join(require.resolve("@skeletonlabs/skeleton"), "../**/*.{html,js,svelte,ts}"),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-} 
+  plugins: [
+    skeleton({
+      themes: { preset: ["skeleton"] }, // Используем базовую тему Skeleton
+    }),
+  ],
+};
