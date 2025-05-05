ea1aea541bafd819d1e68e10ba441b30fa2ce526
agolovachev <antongolova@gmail.com>
2025-05-06 02:53:27 +0700

    chore: обновление зависимостей проекта
    1. Добавлена библиотека svelte-heros-v2 в package.json
    
    2. Обновлен tailwind.config.js
    
    3. Обновлен файл блокировки зависимостей pnpm-lock.yaml

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,10 @@
-import { skeleton } from "@skeletonlabs/tw-plugin";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./src/**/*.{html,js,svelte,ts}", // Указываем пути для сканирования
-    // Удаляем путь к файлам Skeleton UI
-    // require("path").join(require.resolve("@skeletonlabs/skeleton"), "../**/*.{html,js,svelte,ts}"),
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    // Удаляем плагин Skeleton
-    // skeleton({
-    //   themes: { preset: ["skeleton"] }, // Используем базовую тему Skeleton
-    // }),
-  ],
+  plugins: [],
 };
